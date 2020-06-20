1b3f277528d17bc06ad435ca08e0ce2629a59509
Michael R <mruhaak@gmail.com>
2020-06-20 05:46:10 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
-def parrot(sound = "Squawk!")
-  puts "The sound a parrot makes is #{sound}"
+def parrot(sound = "Squawk")
+  puts "The sound a parrot makes is #{sound}!"
 end
 
 # Create method `parrot` that outputs a given phrase and
